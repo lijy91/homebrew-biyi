ed5dc964439e7305f18c6460e38bb04d83957dd9
李健莹 <lijy91@foxmail.com>
2023-06-04 08:11:22 +0800

    biyi 0.5.0

--- Casks/biyi.rb
+++ Casks/biyi.rb
@@ -1,14 +1,12 @@
 cask "biyi" do
-    version "0.3.6"
+    version "0.5.0"
     sha256 :no_check
   
-    url "https://github.com/biyidev/biyi_app/releases/download/v0.3.6/biyi-0.3.6+9-macos.zip",
+    url "https://github.com/biyidev/biyi_app/releases/download/v0.5.0/biyi-0.5.0+18-macos.zip",
         verified: "github.com/biyidev/biyi/"
     name "Biyi"
     desc "a convenient translation and dictionary app written in Flutter."
     homepage "https://biyidev.com/"
   
     app "biyi.app"
-  
-    zap trash: "~/.biyi"
   end
